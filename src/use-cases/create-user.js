498f8996f9c50f33293b9d3b15768147f0583b39
TGianoni <gianoni2@hotmail.com>
2025-06-13 10:11:13 -0300

    feat: add CreateUserController and validatiosn

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,6 +1,8 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
 export class CreateUserUseCase {
     async execute(createUserParams) {
         // TODO: verificar se o e-mail já está em uso
@@ -19,7 +21,7 @@ export class CreateUserUseCase {
         }
 
         // Chamar o repositório para salvar o usuário
-        const postgresCreateUserRepository = new postgresCreateUserRepository()
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
         const createdUser = await postgresCreateUserRepository.execute(user)
 
