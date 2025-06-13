70fffcab6a85f80068563a635ed4290d1d969f5c
TGianoni <gianoni2@hotmail.com>
2025-06-13 16:24:47 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,46 @@
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres-get-user-by-email-repository.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import bcrypt from 'bcrypt'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams = {}) {
+        // se o e-mail estiver sendo atualizado, verificar se já existe um usuário com esse e-mail
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProvideEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvideEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        // se a senha estiver sendo atualizada, verificar se a senha atende aos critérios de segurança
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+        // chamar o repositório de atualização de usuário
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updatedUser
+    }
+}
