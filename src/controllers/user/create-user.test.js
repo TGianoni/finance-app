a04c830a9e80b93e3604467196612b50064df5e7
TGianoni <gianoni2@hotmail.com>
2025-07-11 18:05:42 -0300

    test: ensure 400 is returned if password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -111,4 +111,24 @@ describe('Create User Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Thiago',
+                last_name: 'Gianoni',
+                email: 'thiago',
+                password: '123',
+            },
+        }
+
+        //act
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
