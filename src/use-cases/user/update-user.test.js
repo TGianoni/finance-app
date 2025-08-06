23d48b8be19ce64fb368f4f0f2afba5b4522f105
TGianoni <gianoni2@hotmail.com>
2025-08-06 17:10:42 -0300

    test: ensure UpdateUserUseCase throws if UpdateUserRepository throws

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -169,4 +169,22 @@ describe('UpdateUserUseCase', () => {
         // assert
         await expect(result).rejects.toThrow()
     })
+    it('should throw if UpdateUserRepository throws', async () => {
+        // arrange
+        const { sut, updateUserRepository } = makeSut()
+        jest.spyOn(updateUserRepository, 'execute').mockRejectedValue(
+            new Error(),
+        )
+
+        // act
+        const result = sut.execute(faker.string.uuid(), {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            password: faker.internet.password(),
+        })
+
+        // assert
+        await expect(result).rejects.toThrow()
+    })
 })
