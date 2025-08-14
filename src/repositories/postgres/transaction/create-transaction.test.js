7b425ed76ac69469c27af404bdd3032442f7804f
TGianoni <gianoni2@hotmail.com>
2025-08-14 17:29:10 -0300

    test: ensure PostgresCreateTransactionRepository call Prisma with correct params

--- src/repositories/postgres/transaction/create-transaction.test.js
+++ src/repositories/postgres/transaction/create-transaction.test.js
@@ -1,11 +1,11 @@
 import { PostgresCreateTransactionRepository } from './create-transaction'
-import { transaction, user as fakeUser } from '../../../tests/index'
+import { transaction, user } from '../../../tests/index'
 import { prisma } from '../../../../prisma/prisma'
 import dayjs from 'dayjs'
 
 describe('PostgresCreateTransactionRepository', () => {
     it('shoudl create a transaction on db', async () => {
-        const user = await prisma.user.create({ data: fakeUser })
+        await prisma.user.create({ data: user })
 
         const sut = new PostgresCreateTransactionRepository()
 
@@ -20,5 +20,19 @@ describe('PostgresCreateTransactionRepository', () => {
         )
         expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
         expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
+    })
+    it('should call Prisma with correct params', async () => {
+        await prisma.user.create({ data: user })
+        const sut = new PostgresCreateTransactionRepository()
+        const prismaSpy = jest.spyOn(prisma.transaction, 'create')
+
+        await sut.execute({ ...transaction, user_id: user.id })
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            data: {
+                ...transaction,
+                user_id: user.id,
+            },
+        })
     })
 })
