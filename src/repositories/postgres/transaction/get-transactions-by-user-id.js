788c392aa1dbc61a81eb480e112182e73bbd4021
TGianoni <gianoni2@hotmail.com>
2025-08-14 16:27:41 -0300

    fix: ensure PostgresGetTransactionsByUserIdRepository return a transaction where user_id

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -2,9 +2,9 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionsByUserIdRepository {
     async execute(userId) {
-        return await prisma.transactions.findMany({
+        return await prisma.transaction.findMany({
             where: {
-                id: userId,
+                user_id: userId,
             },
         })
     }
