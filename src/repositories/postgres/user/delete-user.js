f5b103292915779bc5c4905b9fe80992b98ca013
TGianoni <gianoni2@hotmail.com>
2025-08-19 10:46:56 -0300

    feat: ensure PostgresDeleteUserRespository throws if Prisma throws

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,4 +1,6 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { prisma } from '../../../../prisma/prisma.js'
+import { UserNotFoundError } from '../../../errors/user.js'
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
@@ -9,7 +11,12 @@ export class PostgresDeleteUserRepository {
                 },
             })
         } catch (error) {
-            return null
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2025') {
+                    throw new UserNotFoundError(userId)
+                }
+            }
+            throw error
         }
     }
 }
