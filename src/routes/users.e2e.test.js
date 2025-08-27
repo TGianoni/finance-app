911d8b38859062c3d47c40c800928453902426ff
TGianoni <gianoni2@hotmail.com>
2025-08-27 10:43:37 -0300

    test: ensure GET /api/users/:userId should returns 404 when user is not found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -110,4 +110,12 @@ describe('User Router E2E Tests', () => {
             balance: '6000',
         })
     })
+
+    it('GET /api/users/:userId should return 404 when user is not found', async () => {
+        const response = await request(app).get(
+            `/api/users/${faker.string.uuid()}`,
+        )
+
+        expect(response.status).toBe(404)
+    })
 })
