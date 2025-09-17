e34e06de828f46e9a08e5c48f5808db3b59c63a6
TGianoni <gianoni2@hotmail.com>
2025-09-17 15:53:31 -0300

    feat: add authentication middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+
+export const auth = (request, response, next) => {
+    try {
+        // pegar o access token do header
+        const accessToken = request.headers?.authorization?.split('Bearer ')[1]
+        if (!accessToken) {
+            return response.status(401).send({ message: 'Unauthorized' })
+        }
+
+        // verificar se o access token é válido
+        const decodedToken = jwt.verify(
+            accessToken,
+            process.env.JWT_ACCESS_TOKEN_SECRET,
+        )
+        if (!decodedToken) {
+            return response.status(401).send({ message: 'Unauthorized' })
+        }
+        request.userId = decodedToken.userId
+
+        // se for válido, deixar a requisição continuar
+        next()
+
+        // se não for válido, retornar um erro 401
+
+        console.log('auth middleware is running')
+        next()
+    } catch (error) {
+        console.log(error)
+        return response.status(401).send({ message: 'Unauthorized' })
+    }
+}
