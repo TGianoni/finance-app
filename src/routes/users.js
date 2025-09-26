24fa68d00d516b88f19c987d92c6659503693bfa
TGianoni <gianoni2@hotmail.com>
2025-09-26 17:04:32 -0300

    feat: add /transactions/me routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,7 +12,7 @@ import { auth } from '../middlewares/auth.js'
 
 export const usersRouter = Router()
 
-usersRouter.get('/', auth, async (request, response) => {
+usersRouter.get('/me', auth, async (request, response) => {
     const getUserByIdController = makeGetUserByIdController()
 
     console.log('Usuário autenticado: ', request.userId)
@@ -25,7 +25,7 @@ usersRouter.get('/', auth, async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-usersRouter.get('/balance', auth, async (request, response) => {
+usersRouter.get('/me/balance', auth, async (request, response) => {
     const getUserBalanceController = makeGetUserBalanceController()
 
     const { statusCode, body } = await getUserBalanceController.execute({
@@ -50,7 +50,7 @@ usersRouter.post('/', async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-usersRouter.patch('/', auth, async (request, response) => {
+usersRouter.patch('/me', auth, async (request, response) => {
     const updateUserController = makeUpdateUserController()
 
     const { statusCode, body } = await updateUserController.execute({
@@ -61,7 +61,7 @@ usersRouter.patch('/', auth, async (request, response) => {
     response.status(statusCode).send(body) //json é igual a send
 })
 
-usersRouter.delete('/', auth, async (request, response) => {
+usersRouter.delete('/me', auth, async (request, response) => {
     const deleteUserController = makeDeleteUserController()
 
     const { statusCode, body } = await deleteUserController.execute({
